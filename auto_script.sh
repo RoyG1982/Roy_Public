bc5ca099c17058f4ded200c2379bc7185988484a
RoyG1982 <124822650+RoyG1982@users.noreply.github.com>
2024-12-20 18:50:32 +0200

    Update auto_script.sh

--- auto_script.sh
+++ auto_script.sh
@@ -1,10 +1,8 @@
-#!/usr/bin/env pwsh
-
-scp -i "C:\Users\User\Desktop\DevOps\Roy_key.pem" -r "C:\Users\User\Desktop\DevOps\Domain_monitoring7" ubuntu@35.85.35.156:/home/ubuntu/
-
-ssh -i "C:\Users\User\Desktop\DevOps\Roy_key.pem" ubuntu@35.85.35.156 {
-    sudo apt update
-    sudo apt install -y python3-pip python3-flask
-    cd Domain_monitoring7
-    python3 app.py
-}
+#!/bin/bash
+scp -i "/mnt/c/Users/User/Desktop/DevOps/Roy_key.pem" -r "/mnt/c/Users/User/Desktop/DevOps/Domain_monitoring7" ubuntu@35.85.35.156:/home/ubuntu/
+ssh ubuntu@35.85.35.156 -i /mnt/c/Users/User/Desktop/DevOps/Roy_key.pem
+sudo apt update
+sudo apt install python3-pip
+sudo apt install python3-flask
+cd Domain_monitoring7
+python3 app.py
