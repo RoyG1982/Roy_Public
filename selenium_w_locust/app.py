eb6ddfdba4cb0e27c122fe4f1f9eb7a0abb39fd6
unknown <royginat@gmail.com>
2025-01-01 13:02:39 +0200

    selenium_w_locust

--- selenium_w_locust/app.py
+++ selenium_w_locust/app.py
@@ -1,16 +1,15 @@
 from locust import HttpUser, task, between
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
 class SeleniumUser(HttpUser):
     wait_time = between(1, 5)
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        HttpUser.__init__(self, *args, **kwargs)
         self.driver = None
 
     def on_start(self):
@@ -22,7 +21,7 @@ class SeleniumUser(HttpUser):
 
     @task
     def go_to_user_homepage(self):
-        self.driver.get("http://127.0.0.1:8080/")
+        self.driver.get("http://127.0.0.1:8080")
 
         l_username = self.driver.find_element(By.ID, "l_username")
         l_password = self.driver.find_element(By.ID, "l_password")
@@ -32,9 +31,19 @@ class SeleniumUser(HttpUser):
         l_password.send_keys("123")
         login_button.click()
 
-        # Wait until the user_homepage page is fully loaded after login
         WebDriverWait(self.driver, 10).until(
             EC.url_to_be("http://127.0.0.1:8080/user_homepage")
         )
 
         self.client.get("/user_homepage")
+
+    @task
+    def back_to_home(self):
+        back_to_home = self.driver.find_element(By.ID, "back_to_home")
+        back_to_home.click()
+
+        WebDriverWait(self.driver, 10).until(
+            EC.url_to_be("http://127.0.0.1:8080/")
+        )
+
+        self.client.get("/")
