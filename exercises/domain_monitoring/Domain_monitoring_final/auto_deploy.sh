545c146a93803837d63d53b9afa3a9ee982499b6
root <root@DESKTOP-476I4C0.>
2025-03-31 14:53:37 +0300

    deploy

--- exercises/domain_monitoring/Domain_monitoring_final/auto_deploy.sh
+++ exercises/domain_monitoring/Domain_monitoring_final/auto_deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-
+# git clone https://github.com/RoyG1982/Roy_Public.git
+# cd /Roy_Public/exercises/domain_monitoring/Domain_monitoring_final
 sudo apt update
 sudo apt install python3-pip
 pip3 install flask --break-system-packages
+python3 app.py
