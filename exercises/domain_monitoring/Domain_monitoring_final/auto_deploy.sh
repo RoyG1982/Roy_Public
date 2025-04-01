d3e7262f2e5f1b9781a83a0e29d421d7c9261bde
root <root@DESKTOP-476I4C0.>
2025-04-01 21:14:03 +0300

    deploy

--- exercises/domain_monitoring/Domain_monitoring_final/auto_deploy.sh
+++ exercises/domain_monitoring/Domain_monitoring_final/auto_deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# git clone https://github.com/RoyG1982/Roy_Public.git
-# cd /Roy_Public/exercises/domain_monitoring/Domain_monitoring_final
+git clone https://github.com/RoyG1982/Roy_Public.git
 sudo apt update
-sudo apt install python3-pip
+sudo apt install python3-pip -y
 pip3 install flask --break-system-packages
+cd Roy_Public/exercises/domain_monitoring/Domain_monitoring_finalls
 python3 app.py
---
+++ exercises/domain_monitoring/Domain_monitoring_final/auto_deploy_with_service_addition.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+REPO_URL="https://github.com/RoyG1982/Roy_Public.git"
+APP_DIR="Roy_Public/exercises/domain_monitoring/Domain_monitoring_final"
+SERVICE_NAME="flaskapp.service"
+USERNAME="ubuntu"
+
+git clone "$REPO_URL" || exit 1  // #|| =  or exit with error 1
+
+cd "$APP_DIR" || exit 1
+
+sudo apt update || exit 1
+sudo apt install -y python3-pip || exit 1
+
+pip3 install flask --break-system-packages || exit 1
+
+#This creates a systemd service file at /etc/systemd/system/flaskapp.service:
+#[Unit]: Defines the service description and ensures it starts after the network is up.
+
+echo "[Unit]
+Description=Flask App Deployment
+After=network.target
+
+[Service]
+User=$USERNAME
+WorkingDirectory=$(pwd)
+ExecStart=/usr/bin/python3 app.py
+Restart=always
+
+[Install]
+WantedBy=multi-user.target" | sudo tee /etc/systemd/system/"$SERVICE_NAME"
+
+sudo systemctl daemon-reload #Reloads systemd to recognize the new service.
+sudo systemctl enable "$SERVICE_NAME" #Enables the service to start on boot.
+sudo systemctl start "$SERVICE_NAME" #Starts the Flask application.
+sudo systemctl status "$SERVICE_NAME"
